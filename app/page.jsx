1464319c16421cecbf5e3f5e269cfe5f8a8a700e
rezafikkri <fikkri.reza@gmail.com>
2025-03-01 18:28:36 +0700

    feat: change the logo to cloudinary

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Image from 'next/image';
 import { useState } from 'react';
 
 export default function Home() {
@@ -14,12 +13,7 @@ export default function Home() {
           <svg className="absolute text-gray-800 icon block -bottom-3.5 ms-6" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 9c.852 0 1.297 .986 .783 1.623l-.076 .084l-6 6a1 1 0 0 1 -1.32 .083l-.094 -.083l-6 -6l-.083 -.094l-.054 -.077l-.054 -.096l-.017 -.036l-.027 -.067l-.032 -.108l-.01 -.053l-.01 -.06l-.004 -.057v-.118l.005 -.058l.009 -.06l.01 -.052l.032 -.108l.027 -.067l.07 -.132l.065 -.09l.073 -.081l.094 -.083l.077 -.054l.096 -.054l.036 -.017l.067 -.027l.108 -.032l.053 -.01l.06 -.01l.057 -.004l12.059 -.002z" /></svg>
         </div>
         <div className="flex items-center justify-center">
-          <Image
-            src="/PriskoJrMod.png"
-            alt="Prisko Jr Mod Logo"
-            width={50}
-            height={50}
-          />
+          <img src="https://res.cloudinary.com/priskojrmod/image/upload/q_auto/PriskoJrMod.png" height={50} width={50} alt="Prisko Jr Mod Logo" loading="lazy" decoding="async" />
           <h1 className="font-bold text-4xl ms-4">Prisko Jr Mod</h1>
         </div>
       </div>
