5f6aeccb90781154f8a9e28b45ed40307cfc1653
rezafikkri <fikkri.reza@gmail.com>
2025-03-08 16:16:03 +0700

    feat: header blur and background

--- components/layout/header.jsx
+++ components/layout/header.jsx
@@ -2,7 +2,7 @@ import Nav from './nav';
 
 export default function Header() {
   return (
-    <header className="text-white-n sticky top-0 bg-black-m z-99 w-full">
+    <header className="text-white-n sticky top-0 bg-black-m/90 backdrop-blur-lg z-99 w-full">
       <div className="w-full lg:max-w-6xl mx-auto flex justify-between">
         <a href="" className="flex items-center font-extrabold text-xl py-4 px-2">
           <img src="https://res.cloudinary.com/priskojrmod/image/upload/q_auto/PriskoJrMod.png" alt="Prisko Jr Mod Logo" width={28} height={28} />
--- components/layout/nav.jsx
+++ components/layout/nav.jsx
@@ -19,16 +19,16 @@ export default function Nav() {
           <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 8l16 0" /><path d="M4 16l16 0" /></svg>
         }
       </button>
-      <nav className={`[&_a]:inline-block [&_svg]:inline-block font-bold items-center ${showNav ? 'block absolute right-0 bg-gray-900 h-screen pt-15 p-6':'hidden'} md:h-auto md:pt-0 md:p-0 md:flex md:static md:bg-transparent`}>
+      <nav className={`[&_a]:inline-block [&_svg]:inline-block font-bold items-center ${showNav ? 'block absolute right-0 bg-[#141414] h-screen pt-15 p-6':'hidden'} md:h-auto md:pt-0 md:p-0 md:flex md:static md:bg-transparent`}>
         <ul className="[&>li]:py-2 mb-10 md:mb-0 [&>li]:md:px-4 [&>li]:lg:px-6 md:me-10 lg:me-20 [&>li]:md:inline-block">
-          <li><a href="">Products</a></li>
-          <li>
+          <li><a href="#">Products</a></li>
+          <li className="relative">
             <button onClick={() => setShowServices(prev => !prev)} type="button">
               <span className="me-1">Services</span>
               <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} viewBox="0 0 24 24" fill="currentColor" className="icon"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 9c.852 0 1.297 .986 .783 1.623l-.076 .084l-6 6a1 1 0 0 1 -1.32 .083l-.094 -.083l-6 -6l-.083 -.094l-.054 -.077l-.054 -.096l-.017 -.036l-.027 -.067l-.032 -.108l-.01 -.053l-.01 -.06l-.004 -.057v-.118l.005 -.058l.009 -.06l.01 -.052l.032 -.108l.027 -.067l.07 -.132l.065 -.09l.073 -.081l.094 -.083l.077 -.054l.096 -.054l.036 -.017l.067 -.027l.108 -.032l.053 -.01l.06 -.01l.057 -.004l12.059 -.002z" /></svg>
             </button>
             {showServices &&
-              <ul className="absolute">
+              <ul className="-left-25 md:left-0 -right-3 md:-right-35 lg:-right-30 top-11 md:top-13 absolute bg-black-s [&_a]:block [&_a]:px-5 py-5 space-y-5 rounded-xl shadow-lg">
                 <li><a href="">Upgrade License</a></li>
                 <li><a href="">Download Default Addon</a></li>
               </ul>
