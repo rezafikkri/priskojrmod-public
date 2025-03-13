16c5499723e37206c7d1294408c6391a15a57e08
rezafikkri <fikkri.reza@gmail.com>
2025-03-13 08:59:39 +0700

    feat: improve layout size for responsive

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,9 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
-      <body className={montserrat.variable}>
+      <body className={`${montserrat.variable} flex flex-col min-h-screen`}>
         <Header />
-        <main>{children}</main>
+        <main className="flex-1">{children}</main>
         <Footer />
       </body>
     </html>
